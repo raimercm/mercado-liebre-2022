8577db4865d9fb78280329c4de68e87b903702c1
raimercm <raimercm@gmail.com>
2022-03-15 19:59:44 -0500

    primer commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express=require("express")
+const path=require('path')
+const app=express()
+
+//const publicPath=path.resolve(__dirname,'../public')
+//app.use(express.static(publicPath))
+app.use(express.static(path.join(__dirname,"../","public")));
+//app.use(express.static('../public'));
+
+
+app.listen(3000,()=>console.log('ejecutando'))
+
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'/views/home.html')))
+app.get('/register',(req,res)=>res.sendFile(path.join(__dirname,'/views/register.html')))
+app.get('/login',(req,res)=>res.sendFile(path.join(__dirname,'/views/login.html')))
